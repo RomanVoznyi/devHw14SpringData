47e733cdecacfb2a3a3351e122bb738abee2f2a1
RomanVoznyy <roman.voznyy.52@gmail.com>
2023-03-26 18:04:10 +0300

    added spring data project

---
+++ src/main/java/homework/service/NoteService.java
@@ -0,0 +1,46 @@
+package homework.service;
+
+import homework.model.Note;
+import homework.model.NoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class NoteService {
+    private final NoteRepository noteRepository;
+
+    public List<Note> listAll() {
+        return noteRepository.findAll();
+    }
+
+    public Note add(Note note) {
+        return noteRepository.save(note);
+    }
+
+    public boolean isExist(long id) {
+       return noteRepository.existsById(id);
+    }
+
+    public void deleteById(long id) {
+        if(!isExist(id)){
+            throw new IllegalArgumentException("Note with id '" + id + "' does not exist");
+        }
+        noteRepository.deleteById(id);
+    }
+
+    public void update(Note note) {
+       noteRepository.save(note);
+    }
+
+    public Note getById(long id) {
+        if(!isExist(id)){
+            throw new IllegalArgumentException("Note with id '" + id + "' does not exist");
+        }
+        return noteRepository.getReferenceById(id);
+    }
+}
